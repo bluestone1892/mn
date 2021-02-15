66fab61e41d35077e9568d8f75e76ff217a1f6c0
bluestone1892 <75886825+bluestone1892@users.noreply.github.com>
2021-02-15 16:16:42 +0900

    Add files via upload

---
+++ mnbot.py
@@ -0,0 +1,35 @@
+import discord
+import asyncio, random, datetime, asyncio, json
+from discord.ext import commands
+
+client = discord.Client()
+bot = commands.Bot(command_prefix="m ")
+
+@bot.event
+async def on_ready():
+    print('샌즈')
+    print(bot.user.id)
+    await bot.change_presence(
+    status=discord.Status.online,
+    activity=discord.Game("접두사는 m"))
+
+@bot.command(name="핑")
+async def ping(ctx):
+  embed = discord.Embed(title=':ping_pong: 퐁!', description=f'{round(round(bot.latency, 4)*1000)}ms', color=0x00aaaa)
+  await ctx.send(embed=embed)
+@bot.command(name= "초대링크")
+async def invite(ctx):
+  embed = discord.Embed(title='아쉽지만 m봇은 엠엔네이션에서만 사용할수있어요', description="( ˘︹˘ )", color=0x00aaaa)
+  await ctx.send(embed=embed)
+@bot.command(name= "안녕")
+async def hi(ctx):
+  embed = discord.Embed(title='안녕하세요!', description=" 안녕하세요!", color=0x00aaaa)
+  await ctx.send(embed=embed)
+@bot.command(name= "엠엔")
+async def mn(ctx):
+  embed = discord.Embed(title='엠엔님은 서버 주인입니다!', description="||아주 귀여브시죠!||", color=0x00aaaa)
+  await ctx.send(embed=embed)
+
+
+
+bot.run('Nzk2OTI0NDE5NjYyMDIwNjA5.X_e_sw.1DD9qUJ6GuPtg6AQWv8shsA_njQ')
